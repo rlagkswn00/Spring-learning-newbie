b0614165bc1372a34d4582372f32438da3a81018
jiwoo <zbqmgldjfh@gmail.com>
2023-10-18 23:10:20 +0900

    init commit

---
+++ src/main/java/gdsc/shine/springlearningsimple/SpringLearningSimpleApplication.java
@@ -0,0 +1,13 @@
+package gdsc.shine.springlearningsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearningSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLearningSimpleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gdsc/shine/springlearningsimple/SpringLearningSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package gdsc.shine.springlearningsimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearningSimpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
