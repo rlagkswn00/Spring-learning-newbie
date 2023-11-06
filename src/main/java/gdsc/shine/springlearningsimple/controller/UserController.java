e1ca32da9430db57eea522f9c7e5710ff0ce6d22
Han Ju Kim <hjkim2902@gmail.com>
2023-11-06 22:50:08 +0900

    refactor : 매직넘버 사용 & 불필요 로깅 제거

--- src/main/java/gdsc/shine/springlearningsimple/controller/UserController.java
+++ src/main/java/gdsc/shine/springlearningsimple/controller/UserController.java
@@ -2,31 +2,34 @@ package gdsc.shine.springlearningsimple.controller;
 
 
 import gdsc.shine.springlearningsimple.dao.UserDAO;
-import gdsc.shine.springlearningsimple.domain.User;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class UserController {
+    private static final int INIT_COUNT = 1;
 
     private final UserDAO userDAO;
 
     @GetMapping("/users")
     public ResponseEntity<String> findUserByName(@RequestParam String name) {
-        log.info("UserController.findUserByName");
-
         userDAO.insertUserByName(name);
 
         int visitCount = userDAO.countByUserName(name);
 
+        if(visitCount == -1){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("잘못된 요청입니다.");
+        }
+
+        if (visitCount == INIT_COUNT){
             return ResponseEntity.ok("안녕하세요! " + name);
+        }
 
         return ResponseEntity.ok(name + "님 안녕하세요, " + visitCount + "번째 방문이시군요!");
     }
