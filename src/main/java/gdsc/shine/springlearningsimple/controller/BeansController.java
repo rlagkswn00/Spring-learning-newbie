aa1b6226c3f44bb990bcb010bda9898c48b590af
Han Ju Kim <hjkim2902@gmail.com>
2023-11-06 21:50:10 +0900

    refactor : 불필요 어노테이션 제거

--- src/main/java/gdsc/shine/springlearningsimple/controller/BeansController.java
+++ src/main/java/gdsc/shine/springlearningsimple/controller/BeansController.java
@@ -1,14 +1,10 @@
 package gdsc.shine.springlearningsimple.controller;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Enumeration;
-
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class BeansController {
