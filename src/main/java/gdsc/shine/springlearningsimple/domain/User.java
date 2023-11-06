285200869edf65935d35bcda49bf8c3095f8b816
Han Ju Kim <hjkim2902@gmail.com>
2023-11-06 22:49:44 +0900

    feat : @Data 대신 필요한 어노테이션 사용

--- src/main/java/gdsc/shine/springlearningsimple/domain/User.java
+++ src/main/java/gdsc/shine/springlearningsimple/domain/User.java
@@ -2,12 +2,12 @@ package gdsc.shine.springlearningsimple.domain;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
+@Getter
 @NoArgsConstructor
 public class User {
     private Long id;
