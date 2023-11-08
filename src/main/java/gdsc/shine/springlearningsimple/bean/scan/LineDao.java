2f703799ca18cf40455955388d1396205b872c0d
Han Ju Kim <hjkim2902@gmail.com>
2023-11-08 17:10:06 +0900

    feat : LineDao Bean 등록

--- src/main/java/gdsc/shine/springlearningsimple/bean/scan/LineDao.java
+++ src/main/java/gdsc/shine/springlearningsimple/bean/scan/LineDao.java
@@ -1,4 +1,8 @@
 package gdsc.shine.springlearningsimple.bean.scan;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+@Component("lineDao")
 public class LineDao {
 }
