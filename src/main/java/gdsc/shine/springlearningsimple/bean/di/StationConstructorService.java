6083fdf0969aec05e37eb045e3211b0a63e3150c
Han Ju Kim <hjkim2902@gmail.com>
2023-11-08 17:10:49 +0900

    feat : Constructor 주입 Autowired 설정

--- src/main/java/gdsc/shine/springlearningsimple/bean/di/StationConstructorService.java
+++ src/main/java/gdsc/shine/springlearningsimple/bean/di/StationConstructorService.java
@@ -1,7 +1,16 @@
 package gdsc.shine.springlearningsimple.bean.di;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class StationConstructorService {
     private StationRepository stationRepository;
+
+    @Autowired
+    public StationConstructorService(StationRepository stationRepository) {
+        this.stationRepository = stationRepository;
+    }
 
     public String sayHi() {
         return stationRepository.sayHi();
