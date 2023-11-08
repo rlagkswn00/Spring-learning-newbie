4042fc5e418f9055ac9c7a98b7466de1dad250c8
Han Ju Kim <hjkim2902@gmail.com>
2023-11-08 17:11:17 +0900

    feat : ConsolePrinter Bean 등록

--- src/main/java/gdsc/shine/springlearningsimple/bean/context/ConsolePrinter.java
+++ src/main/java/gdsc/shine/springlearningsimple/bean/context/ConsolePrinter.java
@@ -1,5 +1,8 @@
 package gdsc.shine.springlearningsimple.bean.context;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ConsolePrinter implements Printer {
     @Override
     public void print(String message) {
