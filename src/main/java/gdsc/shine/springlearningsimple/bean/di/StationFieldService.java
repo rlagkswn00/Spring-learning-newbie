12c480da067a162d30e29aa162b53fa96d401c66
Han Ju Kim <hjkim2902@gmail.com>
2023-11-08 17:10:42 +0900

    feat : Field 주입 Autowired 설정

--- src/main/java/gdsc/shine/springlearningsimple/bean/di/StationFieldService.java
+++ src/main/java/gdsc/shine/springlearningsimple/bean/di/StationFieldService.java
@@ -1,6 +1,11 @@
 package gdsc.shine.springlearningsimple.bean.di;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class StationFieldService {
+    @Autowired
     private StationRepository stationRepository;
 
     public String sayHi() {
