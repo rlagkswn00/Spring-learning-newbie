b596a47f04b596f4aec8a044e2ba912ea0fc8672
Han Ju Kim <hjkim2902@gmail.com>
2023-11-18 14:20:19 +0900

    feat : Set ParamHeaderController

--- src/main/java/gdsc/shine/springlearningsimple/mvc/mapping/ParamHeaderController.java
+++ src/main/java/gdsc/shine/springlearningsimple/mvc/mapping/ParamHeaderController.java
@@ -1,22 +1,29 @@
 package gdsc.shine.springlearningsimple.mvc.mapping;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/param-header")
 public class ParamHeaderController {
 
+    @GetMapping(value = "/message",produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> message() {
         return ResponseEntity.ok().body("message");
     }
 
-    public ResponseEntity<String> messageForParam() {
-        return ResponseEntity.ok().body("hello");
+    @GetMapping(value = "/message",produces = MediaType.APPLICATION_JSON_VALUE, params = "name=hello")
+    public ResponseEntity<String> messageForParam(@RequestParam("name") String name) {
+        return ResponseEntity.ok().body(name);
     }
 
-    public ResponseEntity<String> messageForHeader() {
-        return ResponseEntity.ok().body("hi");
+    @GetMapping(value = "/message",produces = MediaType.APPLICATION_JSON_VALUE, headers = "HEADER=hi")
+    public ResponseEntity<String> messageForHeader(@RequestHeader("HEADER") String header) {
+        return ResponseEntity.ok().body(header);
     }
 }
