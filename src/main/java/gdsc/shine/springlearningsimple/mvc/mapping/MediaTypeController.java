47f030fb2320731bb90f65e945c24668c7f013f2
Han Ju Kim <hjkim2902@gmail.com>
2023-11-18 14:20:06 +0900

    feat : Set MediaTypeController

--- src/main/java/gdsc/shine/springlearningsimple/mvc/mapping/MediaTypeController.java
+++ src/main/java/gdsc/shine/springlearningsimple/mvc/mapping/MediaTypeController.java
@@ -1,7 +1,10 @@
 package gdsc.shine.springlearningsimple.mvc.mapping;
 
 import gdsc.shine.springlearningsimple.mvc.domain.User;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +17,13 @@ import java.util.List;
 @RequestMapping("/media-type")
 public class MediaTypeController {
 
+    @PostMapping(value = "/users", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity createUser(@RequestBody User user) {
         Long id = 1L;
         return ResponseEntity.created(URI.create("/users/" + id)).build();
     }
+
+    @GetMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
 
     public ResponseEntity<List<User>> showUser() {
         List<User> users = Arrays.asList(
@@ -27,6 +33,7 @@ public class MediaTypeController {
         return ResponseEntity.ok().body(users);
     }
 
+    @GetMapping(value = "/users", produces = MediaType.TEXT_HTML_VALUE)
     public String userPage() {
         return "user page";
     }
