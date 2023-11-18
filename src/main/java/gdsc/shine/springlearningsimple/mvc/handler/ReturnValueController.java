470f1baa9dab3f7130aa10d5920aa7908fc5d49e
Han Ju Kim <hjkim2902@gmail.com>
2023-11-18 14:20:56 +0900

    feat : Set ReturnValueController

--- src/main/java/gdsc/shine/springlearningsimple/mvc/handler/ReturnValueController.java
+++ src/main/java/gdsc/shine/springlearningsimple/mvc/handler/ReturnValueController.java
@@ -1,29 +1,41 @@
 package gdsc.shine.springlearningsimple.mvc.handler;
 
+import gdsc.shine.springlearningsimple.mvc.domain.User;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/return-value")
 public class ReturnValueController {
 
-//    public void string() {
-//        return "message";
-//    }
-//
-//    public void responseBodyForUser() {
-//        return new User("name", "email");
-//    }
-//
-//    public void responseEntity(@PathVariable Long id) {
-//        return ResponseEntity.ok(new User("name", "email"));
-//    }
-//
-//    public void responseEntityFor400() {
-//        return ResponseEntity.badRequest().build();
-//    }
-//
-//    public void thymeleaf() {
-//        return "sample";
-//    }
+    @GetMapping("/message")
+    @ResponseBody
+    public String string() {
+        return "message";
+    }
+
+    @GetMapping("/users")
+    @ResponseBody
+    public User responseBodyForUser() {
+        return new User("name", "email");
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> responseEntity(@PathVariable Long id) {
+        return ResponseEntity.ok(new User("name", "email"));
+    }
+
+    @GetMapping("/members")
+    public ResponseEntity<Object> responseEntityFor400() {
+        return ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping("/thymeleaf")
+    public String thymeleaf() {
+        return "sample";
+    }
 }
