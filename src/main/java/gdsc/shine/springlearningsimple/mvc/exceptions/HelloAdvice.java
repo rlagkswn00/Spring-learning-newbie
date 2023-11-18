bca70532b360dd0799670d87c4e1f2c08cbc7efd
Han Ju Kim <hjkim2902@gmail.com>
2023-11-18 14:21:15 +0900

    feat : Set ControllerAdvice

--- src/main/java/gdsc/shine/springlearningsimple/mvc/exceptions/HelloAdvice.java
+++ src/main/java/gdsc/shine/springlearningsimple/mvc/exceptions/HelloAdvice.java
@@ -1,8 +1,14 @@
 package gdsc.shine.springlearningsimple.mvc.exceptions;
 
+import gdsc.shine.springlearningsimple.mvc.exceptions.exception.HelloException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@ControllerAdvice
 public class HelloAdvice {
+
+    @ExceptionHandler(HelloException.class)
     public ResponseEntity<String> handle() {
         return ResponseEntity.badRequest().body("HelloException");
     }
