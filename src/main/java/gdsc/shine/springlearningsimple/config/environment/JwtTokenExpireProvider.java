4a47086599ff64f4f8be7866884c59f6d56618a3
Han Ju Kim <hjkim2902@gmail.com>
2023-12-04 18:10:11 +0900

    feat : Set JwtTokenExpireProvider

--- src/main/java/gdsc/shine/springlearningsimple/config/environment/JwtTokenExpireProvider.java
+++ src/main/java/gdsc/shine/springlearningsimple/config/environment/JwtTokenExpireProvider.java
@@ -1,11 +1,18 @@
 package gdsc.shine.springlearningsimple.config.environment;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
 // TODO: 컴포넌트 스캔을 통한 빈 등록
+@ConfigurationProperties
+@PropertySource("classpath:application.properties")
 public class JwtTokenExpireProvider {
     // TODO: application.properties의 security.jwt.token.expire-length 값을 활용하여 validityInMilliseconds값 초기화 하기
     private long validityInMilliseconds;
 
-    public JwtTokenExpireProvider(long validityInMilliseconds) {
+    public JwtTokenExpireProvider(@Value("${security.jwt.token.expire-length}")long validityInMilliseconds) {
         this.validityInMilliseconds = validityInMilliseconds;
     }
 
